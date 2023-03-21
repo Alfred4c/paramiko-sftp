a3486b05316a9ef647252f5a8818597ec30af5f0
Alfred4c <109122219+Alfred4c@users.noreply.github.com>
2023-03-21 14:27:37 +0800

    Add files via upload

---
+++ asyncssh-sftp.py
@@ -0,0 +1,34 @@
+import asyncssh
+import asyncio
+import time
+
+
+async def copy_file(source, dest):
+    async with asyncssh.connect('10.0.10.141', username='root') as source_conn:
+        async with asyncssh.connect('10.0.10.141', port=2022, username='testuser', password='tiger') as dest_conn:
+            async with source_conn.start_sftp_client() as source_sftp:
+                async with dest_conn.start_sftp_client() as dest_sftp:
+                    async with source_sftp.open(source, 'rb') as source_file:
+                        source_stat = await source_sftp.stat(source)
+                        file_size = source_stat.size
+                        start_time = time.monotonic()
+                        async with dest_sftp.open(dest, 'wb') as dest_file:
+                            while True:
+                                chunk = await source_file.read(4096)
+                                if not chunk:
+                                    break
+                                await dest_file.write(chunk)
+                        end_time = time.monotonic()
+                        elapsed_time = end_time - start_time
+                        print(
+                            f"File Name:go1.20.2.linux-amd64.tar.gz\n"
+                            f"Size: {file_size} bytes. Time elapsed: {elapsed_time} seconds\n"
+                            f". Chunk size: 4096 bytes.")
+
+
+async def main():
+    await copy_file('/root/test/go1.20.2.linux-amd64.tar.gz', 'go1.20.2.linux-amd64.tar.gz')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
